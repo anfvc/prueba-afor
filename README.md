5727b3086e43c138f528ebc23dd1017954b93fd4
andresvillay <andresvillay2@gmail.com>
2024-11-07 12:58:48 +0100

    finished test

---
+++ README.md
@@ -0,0 +1,24 @@
+# Prueba Afor
+
+## Objetivo
+
+Crear una aplicación web que permita al usuario ingresar una ubicación y obtener el clima actual de esa área utilizando la API de OpenWeatherMap. La prueba se realizará con HTML, CSS, JavaScript.
+
+### Requisitos
+
+**Interfaz de Usuario**
+
+- Un campo de entrada para que el usuario ingrese el nombre de la ciudad.
+- Un botón para enviar la consulta.
+- Un área para mostrar los resultados del clima (temp, condiciones meetereológicas, humedad, etc.)
+- Manejo de errores (ciudad no encontrada)
+
+**API del Clima:**
+
+Utilizar la API de OpenWeatherMap (https://openweathermap.org/api) para obtener los datos metereológicos.
+Necesitarás registrarte y obtener una clave (api key).
+
+**Funciones**
+
+- Hacer una llamada a la API para enviar el formulario.
+- Procesar la respuesta y mostrar los datos en la interfaz.
---
+++ main.js
@@ -0,0 +1,61 @@
+const formulario = document.querySelector("form");
+const container = document.querySelector(".container");
+const temperatura = document.querySelector(".temp");
+const humedad = document.querySelector(".humedad");
+const resultados = document.querySelector("h2");
+const error = document.querySelector(".error");
+// console.log(form);
+
+const getWeatherData = async (ciudad) => {
+  try {
+    const apiKey = "50df9374f23a88514c92812cc968e399";
+    const respuesta = await fetch(
+      `https://api.openweathermap.org/data/2.5/find?q=${ciudad}&units=metric&appid=${apiKey}`
+    );
+
+    // console.log(respuesta);
+
+    if (respuesta.ok) {
+      const data = await respuesta.json();
+      // console.log(data.list[0]);
+      return data.list[0];
+    } else {
+      console.log("We couldn't fetch the data.");
+    }
+  } catch (error) {
+    alert("Something has ocurred");
+  }
+};
+
+formulario.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const ciudad = document.querySelector("input").value;
+
+  if (!ciudad) {
+    error.innerHTML = "Por favor ingrese una ciudad.";
+    error.style.color = "red";
+    // console.log(error);
+    return;
+  }
+
+  error.innerHTML = "";
+
+  const data = await getWeatherData(ciudad);
+
+  if (!data) {
+    error.innerHTML = "We couldn't find this city.";
+    error.style.color = "red";
+    // console.log(error);
+  } else {
+    // console.log(data.main);
+    const temp = data.main.temp;
+    const humidityValue = data.main.humidity;
+    console.log("Temperatura: ", temp);
+    console.log("Humedad:", humidityValue);
+    resultados.style.display = "block";
+    temperatura.innerHTML = `Temperatura: ${temp}°C`;
+    humedad.innerHTML = `Humedad: ${humidityValue}%`;
+    error.innerHTML = "";
+  }
+  document.querySelector("input").value = "";
+});
